021a05316d35a81db77dc6c9ea38084b39905b79
Debashis Mishra <debashis.007.m@gmail.com>
2023-11-30 04:13:13 +0530

    Building Client Side. Menu and its styled components created

---
+++ client/src/App.js
@@ -0,0 +1,80 @@
+import styled from 'styled-components';
+import Menu from './components/Menu';
+import Navbar from './components/Navbar';
+
+const Container = styled.div`
+display: flex;
+`
+
+const Main = styled.div`
+flex:7;
+`
+
+const Wrapper = styled.div`
+
+`
+
+
+function App() {
+  return (
+    <Container>
+      <Menu />
+      <Main>
+        <Navbar />
+        <Wrapper>
+          <h1>Video Cards</h1>
+          <h1>Video Cards</h1>
+          <h1>Video Cards</h1>
+          <h1>Video Cards</h1>
+          <h1>Video Cards</h1>
+          <h1>Video Cards</h1>
+          <h1>Video Cards</h1>
+          <h1>Video Cards</h1>
+          <h1>Video Cards</h1>
+          <h1>Video Cards</h1>
+          <h1>Video Cards</h1>
+          <h1>Video Cards</h1>
+          <h1>Video Cards</h1>
+          <h1>Video Cards</h1>
+          <h1>Video Cards</h1>
+          <h1>Video Cards</h1>
+          <h1>Video Cards</h1>
+          <h1>Video Cards</h1>
+          <h1>Video Cards</h1>
+          <h1>Video Cards</h1>
+          <h1>Video Cards</h1>
+          <h1>Video Cards</h1>
+          <h1>Video Cards</h1>
+          <h1>Video Cards</h1>
+          <h1>Video Cards</h1>
+          <h1>Video Cards</h1>
+          <h1>Video Cards</h1>
+          <h1>Video Cards</h1>
+          <h1>Video Cards</h1>
+          <h1>Video Cards</h1>
+          <h1>Video Cards</h1>
+          <h1>Video Cards</h1>
+          <h1>Video Cards</h1>
+          <h1>Video Cards</h1>
+          <h1>Video Cards</h1>
+          <h1>Video Cards</h1>
+          <h1>Video Cards</h1>
+          <h1>Video Cards</h1>
+          <h1>Video Cards</h1>
+          <h1>Video Cards</h1>
+          <h1>Video Cards</h1>
+          <h1>Video Cards</h1>
+          <h1>Video Cards</h1>
+          <h1>Video Cards</h1>
+          <h1>Video Cards</h1>
+          <h1>Video Cards</h1>
+          <h1>Video Cards</h1>
+          <h1>Video Cards</h1>
+          <h1>Video Cards</h1>
+        </Wrapper>
+      </Main>
+    </Container>
+  );
+}
+
+export default App;
